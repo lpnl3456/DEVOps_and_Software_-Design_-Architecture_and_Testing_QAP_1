c5c630f0bf2be826470d84cae7bba77cd4d4d398
lpnl3456 <luke.peddle@keyin.com>
2024-09-25 13:34:30 -0230

    Created the movie class and Implemented the borrowMovie method. Created borrowMovieTest to test the method

--- src/main/java/Movie_Rental/Movie.java
+++ src/main/java/Movie_Rental/Movie.java
@@ -1,4 +1,53 @@
 package Movie_Rental;
 
 public class Movie {
+
+    private String title;
+    private int availableCopies;
+    private double price;
+
+    private int amountBorrowed = 0;
+
+
+    public Movie(String title, int availableCopies, double price){
+        this.title = title;
+        this.availableCopies = availableCopies;
+        this.price = price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean borrowMovie(){
+        if(amountBorrowed == availableCopies){
+            System.out.println("All copies of this movie have been borrowed");
+            return false;
+        }
+        else{
+            amountBorrowed++;
+            return true;
+        }
+    }
 }
--- src/test/java/Movie_Rental_Tests/MovieTest.java
+++ src/test/java/Movie_Rental_Tests/MovieTest.java
@@ -1,4 +1,21 @@
 package Movie_Rental_Tests;
 
+import Movie_Rental.Movie;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MovieTest {
-}
+    @Test
+
+    public void borrowMovieTest(){
+
+        Movie movie = new Movie("Bob", 1,10.99);
+
+        Assertions.assertTrue(movie.borrowMovie());
+
+        Assertions.assertFalse(movie.borrowMovie());
+
+    }
+
+    }
+
